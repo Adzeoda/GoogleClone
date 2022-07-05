53285a058bd67085cb09ab02e7c514fd47a34093
Adjeoda <andydogah@gmail.com>
2022-07-05 16:59:39 +0000

    some_divs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
+import Home from './pages/Home.js';
 
 function App() {
   return (
     // BEM
     <div className="App">
      <h1>Hey chale make we go</h1>
-     {/* Home */}
+     <Home/>
      {/* SearchPage */}
     </div>
   );
 }
 
 export default App;
+ 
