3e79519beee9b5381371a2783c8df6960b372ac0
Adjeoda <andydogah@gmail.com>
2022-07-09 20:59:51 +0000

    have to install router

---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>This is the HomePage</h1>
+       
+      <div className='home_header'>
+        <div className='home_headerLeft'>
+        {/* Link */}
+        {/* link */}
+        </div>
+        <div className='home_headerRight'>
+        {/* link */}
+        {/* link */}
+        {/* Icon */}
+        {/* Avatar */}
+        </div>
+      </div>
+
+      <div className="home_body">
+
+      </div>
+
+    </div>
+  )
+}
+
+export default Home
