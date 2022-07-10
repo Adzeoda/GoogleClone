97dd26db8307bb00f26ffde925cae4b8f9d5ba63
Adjeoda <andydogah@gmail.com>
2022-07-10 01:26:37 +0000

    App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import './App.css';
 import Home from './pages/Home.js';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 
 function App() {
   return (
     // BEM
     <div className="App">
-     <h1>Hey chale make we go</h1>
-     <Home/>
-     {/* SearchPage */}
+      <Router>
+        <switch>
+          <Route path="/search">
+            <h1>SearchPage</h1>
+          </Route>
+          <Route>
+            <Home/>
+            {/* SearchPage */}
+          </Route>
+        </switch>
+      </Router>
     </div>
   );
 }
-
+ 
 export default App;
  
